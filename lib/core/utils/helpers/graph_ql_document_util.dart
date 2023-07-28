e339a247f8f666561effddb33510367b88654d2b
Daeon97 <engelmmanuel@gmail.com>
2023-07-28 10:10:21 +0100

    Removed unnecessary subscription from GraphQL schema and corresponding request and resolver mapping templates, added new list tracker modules query to schema

--- lib/core/utils/helpers/graph_ql_document_util.dart
+++ lib/core/utils/helpers/graph_ql_document_util.dart
@@ -1,50 +1,50 @@
 // ignore_for_file: public_member_api_docs
 
 final class GraphQLDocumentUtil {
+  static String getTrackerModuleQueryDocument({
+    required int id,
+  }) =>
+      '''
+        query GetTrackerModuleQuery {
+          getTrackerModule(id: $id) {
+            id
+            name
+            data {
+              coordinates {
+                hash
+                lat_lng
+              }
+              battery_level
+              timestamp
+            }
+          }
+        }
+      ''';
+
+  static String listTrackerModulesQueryDocument() => '''
+        query ListTrackerModulesQuery {
+          listTrackerModules {
+            id
+            name
+            data {
+              coordinates {
+                hash
+                lat_lng
+              }
+              battery_level
+              timestamp
+            }
+          }
+        }
+      ''';
+
   static String updateTrackerModuleNameMutationDocument({
     required int id,
     required String name,
   }) =>
       '''
-        mutation MyMutation {
+        mutation UpdateTrackerModuleNameMutation {
           updateTrackerModuleName(input: {id: $id, name: $name})
-        }
-      ''';
-
-  static String onGetTrackerModuleSubscriptionDocument({
-    required int id,
-  }) =>
-      '''
-        subscription MySubscription {
-          onGetTrackerModule(id: $id) {
-            id
-            name
-            data {
-              coordinates {
-                hash
-                lat_lng
-              }
-              battery_level
-              timestamp
-            }
-          }
-        }
-      ''';
-
-  static String onListTrackerModulesSubscriptionDocument() => '''
-        subscription MySubscription {
-          onListTrackerModules {
-            id
-            name
-            data {
-              coordinates {
-                hash
-                lat_lng
-              }
-              battery_level
-              timestamp
-            }
-          }
         }
       ''';
 }
